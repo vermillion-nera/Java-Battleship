1726ed029aaf1b69f1aaa82361cd1ab7f6d007b3
Nera <57953845+vermillion-nera@users.noreply.github.com>
2024-03-18 10:01:18 -0500

    This is horrible bruh I forgot to add the Player object to my commit

---
+++ src/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+    private String name = "Player";
+    private final Board board;
+
+    Player (){
+        this.board = new Board();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public boolean checkLoss(){
+        // A player loses if their board contains no surviving ship units.
+        return (board.shipUnitsRemaining() <= 0);
+    }
+}
